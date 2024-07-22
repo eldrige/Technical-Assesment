14166cf3ae0936f3a8eb49f529a3f01595e603f3
Eldrige <peldrige8@gmail.com>
2024-07-22 16:42:22 +0200

    style: finish responsive

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -16,7 +16,7 @@ const Small = () => {
   return (
     <div
       role="region"
-      className="shadow-[0_0_20px_0_#0000001A] bg-white flex flex-col gap-4 items-center justify-center rounded p-4"
+      className="shadow-[0_0_20px_0_#0000001A] w-full max-w-[280px] bg-white flex tablet-min:hidden flex-col gap-4 items-center justify-center rounded p-4"
     >
       <div
         role="banner"
@@ -53,11 +53,9 @@ const Small = () => {
   );
 };
 
-export const Card = () => {
-  const matches = useMediaQuery('(min-width: 768px)');
-
+const Desktop = () => {
   return (
-    <div className="w-full max-w-[900px] desktop-min:max-w-[1200px] shadow-[0_0_20px_0_#0000001A] bg-white flex gap-8 items-center justify-center rounded p-4 pr-[70px]">
+    <div className="hidden w-full max-w-[900px] desktop-min:max-w-[1200px] shadow-[0_0_20px_0_#0000001A] bg-white desktop-min:flex gap-8 items-center justify-center rounded p-4 pr-[70px]">
       <div className="w-full flex items-center justify-center bg-[#03252B] h-32">
         <Image
           src="/grosvenor.svg"
@@ -88,10 +86,65 @@ export const Card = () => {
         role="button"
         aria-label="Click me"
         aria-describedby="card-description"
-        className="max-w-[306px] capitalize w-full text-[20px] font-bold text-white rounded-[10px] p-5 bg-[#009129]"
+        className="max-w-[306px] desktop-min:max-w-[190px] capitalize w-full text-[20px] font-bold text-white rounded-[10px] p-5 bg-[#009129]"
       >
         bet now
       </button>
     </div>
+  );
+};
+
+const Tablet = () => {
+  return (
+    <div className="hidden w-full max-w-[684px] tablet-min:flex desktop-min:hidden  shadow-[0_0_20px_0_#0000001A] bg-white  flex-col gap-8 items-center justify-center rounded p-4">
+      <div className="w-full flex gap-12">
+        <div className="w-full py-[14px] px-[78px] flex items-center justify-center bg-[#03252B]">
+          <Image
+            src="/grosvenor.svg"
+            alt="Grosvenor Logo"
+            width={120}
+            height={120}
+            priority
+          />
+        </div>
+        <div className="flex w-full flex-col justify-between">
+          <div>
+            <h2 className="text-black underline text-[20px]">
+              Grosvenor Casinos
+            </h2>
+            <div className="flex gap-2">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <StarIcon key={index} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <p className="text-black font-semibold text-[20px]">Bonus</p>
+            <div className="text-[28px] w-full flex flex-row gap-2 text-black font-normal">
+              <p>Bet £10</p>
+              <p>Get £400</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        role="button"
+        aria-label="Click me"
+        aria-describedby="card-description"
+        className="capitalize w-full text-[20px] font-bold text-white rounded-[10px] p-5 bg-[#009129]"
+      >
+        bet now
+      </button>
+    </div>
+  );
+};
+
+export const Card = () => {
+  return (
+    <>
+      <Small />
+      <Tablet />
+      <Desktop />
+    </>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Card } from './components/card';
 
 export default function Home() {
   return (
-    <main className="bg-white flex pt-20 items-center min-h-screen flex-col gap-6 p-4">
+    <main className="bg-white flex py-20 items-center min-h-screen flex-col gap-6">
       {Array.from({ length: 3 }).map((_, index) => (
         <Card key={index} />
       ))}
